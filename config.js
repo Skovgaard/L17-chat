422688ceb4fdec1cd787a88ca16b5ead6d70ba7c
Kasper Skovgaard <skovgaardkasper@gmail.com>
2020-10-29 09:34:40 +0100

    Changed uri

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     // databaseURI: 'mongodb://localhost' + '/chatDB', // or MongoDB Atlas connection URI
-    databaseURI: 'mongodb+srv://dbUser:dbUserPassword@cluster0.yhd7v.mongodb.net/<dbname>?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    databaseURI: 'mongodb+srv://dbUser:dbUserPassword@cluster0.yhd7v.mongodb.net/Cluster0?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
     localPort: 8080
 };
 
